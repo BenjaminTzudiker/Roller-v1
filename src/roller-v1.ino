24711d0a6c82ff6f8ce2a9b38e9c5fda7502a55f
Benjamin Tzudiker <benjamintzudiker@macbook-air-5.dhcp.wustl.edu>
2019-01-27 01:45:36 -0600

    Servo tested.

--- src/roller-v1.ino
+++ src/roller-v1.ino
@@ -5,19 +5,30 @@
  * Date: Jan 27 2019
  */
 
-const String topic = "omnaria/roller-v1";
+#define PIN_SERVO D0
+#define PIN_DISPENSE_BUTTON D1
+
+const String TOPIC = "omnaria/roller-v1";
+const unsigned int DISPENSE_DELAY = 500;
 
 Servo servo;
 
 
 void setup() {
 
+  servo.attach(PIN_SERVO);
+  servo.write(175);
+
+}
+
+void loop() {
 
 
 }
 
 
-void loop() {
+void dispense() {
+
 
 
 }
