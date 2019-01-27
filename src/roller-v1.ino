b785d56af3554ccbc595596e51f6078e7a3b54a5
Benjamin Tzudiker <benjamintzudiker@macbook-air-5.dhcp.wustl.edu>
2019-01-27 01:30:11 -0600

    Initial commit.

---
+++ src/roller-v1.ino
@@ -0,0 +1,23 @@
+/*
+ * Project roller-v1
+ * Description: A dice roller controlled by a webpage.
+ * Author: Ben Tzudiker
+ * Date: Jan 27 2019
+ */
+
+const String topic = "omnaria/roller-v1";
+
+Servo servo;
+
+
+void setup() {
+
+
+
+}
+
+
+void loop() {
+
+
+}
